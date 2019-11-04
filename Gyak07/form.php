7f071ae5b4108af443813935dee9f8cb016ea44b
gvikthor <gerely.viktor@gmail.com>
2019-11-04 08:57:13 +0100

    Create form.php

---
+++ Gyak07/form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <title>Form</title>
+</head>
+<body>
+    <?php
+        if(isset($_POST["nev"])){
+            echo "Szia " . $_POST["nev"] . "!";
+        }else{
+    ?>
+        <form action="form.php" method="post">
+            <input name="nev">
+            <input type="submit">
+        </form>
+    <?php
+        }
+    ?>
+</body>
+</html>

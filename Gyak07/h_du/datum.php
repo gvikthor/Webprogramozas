0f8c44bc55b9f91c921035656f6d90ba4b6bbe8d
gvikthor <gerely.viktor@gmail.com>
2019-11-04 15:28:03 +0100

    Create datum.php

---
+++ Gyak07/h_du/datum.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo date("Y. M d. (D) H:i:s");
+    ?>
+</body>
+</html>

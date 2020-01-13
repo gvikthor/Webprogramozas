beeaaf1ef7c934a2d8b35ad72a32d6618840a6c3
gvikthor <gerely.viktor@gmail.com>
2020-01-13 21:29:07 +0100

    Delete volume.js

--- mixer/volume.js
+++
@@ -1,9 +0,0 @@
-let inputs = document.querySelectorAll('.thor-input')
-
-for(let i = 0; i < inputs.length; i++){
-    inputs[i].addEventListener('input',()=>{
-        let span = document.querySelector('#thor-span-'+inputs[i].dataset['id'])
-        span.innerHTML = inputs[i].value;
-        span.style = `background-color: hsl(${100-inputs[i].value},${inputs[i].value}%,50%);`
-    });
-}
